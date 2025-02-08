316cc42318a0962bbb5257af31c294ca0c2935db
abhaysharma2719 <abhay@techcrumb.com>
2025-02-09 00:56:47 +0530

    implement session validation and redirect in login layout

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,3 +1,23 @@
+// app/login/layout.js
+import { getSessionTokenFromCookie, validateSession } from "@/lib/serverUtils";
+import { redirect } from "next/navigation";
+
 export default async function LoginLayout({ children }) {
-  return children
+  try {
+    const sessionToken = await getSessionTokenFromCookie();
+    if (sessionToken) {
+      try {
+        const session = await validateSession(sessionToken);
+        if (session) {
+          // If the session is valid, redirect to home
+          redirect("/");
+        }
+      } catch (e) {
+        redirect("/api/auth/clear-session")
+      }
+    }
+  } catch (e) {
+    console.log("NO SESSION FOUND, SHOWING LOGIN");
+  }
+  return children;
 }
