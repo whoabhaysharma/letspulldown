32f2df7369a517bde23b53491c53ae23271b38f7
abhaysharma2719 <abhay@techcrumb.com>
2025-02-08 21:43:11 +0530

    add user in the layout

--- src/app/(app)/layout.jsx
+++ src/app/(app)/layout.jsx
@@ -1,7 +1,24 @@
 import UserProvider from "@/context/UserProvider";
+import { getSession } from "@/lib/utils";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function Layout({ children }) {
-  const user = {name : "Abhay sharma"}
+  const cookieStore = cookies()
+
+  const sessionToken = (await cookieStore).get("sessionToken")
+  if(!sessionToken){
+    redirect("/login")
+  }
+
+  let user = null
+
+  try{
+    const sessionDetails = await getSession(sessionToken)
+    user = sessionDetails.identities[0]
+  }catch(e){
+    redirect("/login")
+  }
 
   return <UserProvider user={user}>{children}</UserProvider>;
 }
--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,6 +4,27 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export const getSession = async(sessionToken) => {
+  try {
+    const response = await axios.post(
+        process.env.SESSION_API_URL,
+        { sessionToken }, // Sending sessionToken in request body
+        {
+            headers: {
+                "Content-Type": "application/json",
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+            },
+        }
+    );
+
+    return response.data; // Successful response
+} catch (error) {
+    console.error("Session validation failed:", error.response?.data || error.message);
+    throw new Error(error.response?.data?.error || "Failed to validate session");
+}
+}
  
 export const validateSession = async (sessionToken) => {
   try {
